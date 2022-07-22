857bbacd65d72606934ccc98123388748a0a78de
devsathyaops <98995934+devsathyaops@users.noreply.github.com>
2022-07-22 15:49:26 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "server_job" {
-   description = "This is the spot variable"
-   validation {
-      condition = (length(var.server_job)) <=10
-      error_message = "The tag name mustn't exceed 10 characters"
-   }
-}
